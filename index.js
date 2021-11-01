6f6d142ed2393e7c3cb67865518440de5861e8db
isukeaima <asaumi.kei49@gmail.com>
2021-11-01 21:26:50 +0900

    修正

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const input = document.getElementById("input");
 form.addEventListener("submit",function(event){
   event.preventDefault()
   console.log(input.value);
-  console.log(input.value);
 })
