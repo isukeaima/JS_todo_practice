e8e4b9610b34dff89ab57e0d7a1480c251d776a5
isukeaima <asaumi.kei49@gmail.com>
2021-12-02 18:27:23 +0900

    次localStrageからhtml情報を取得

---
+++ index2.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if(todos){
+  forEach (todo =>{
+    
+  })
+}
+
+form.addEventListener("submit",function (event) {
+  event.preventDefault();
+  add();
+});
+
+function add(){
+  let todoText = input.value;
+  if (todoText) {
+  const li = document.createElement("li");
+  li.innerText = todoText;
+  li.classList.add("list-group-item");
+  ul.appendChild(li);
+  input.value = "";
+  saveData();
+  }
+}
+
+function saveData(){
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach(list => {
+    todos.push(list.innerText);
+    localStorage.setItem("todos",JSON.stringify(todos));
+  });
+}
