736c0feede28732b2998995883b7e3611f53af88
isukeaima <asaumi.kei49@gmail.com>
2021-12-08 16:31:24 +0900

    first commit

---
+++ index4.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+form.addEventListener("submit",function(event) {
+  console.log(input.value);
+  event.preventDefault();
+  add();
+});
+
+function add(){
+  const li = document.createElement("li");
+  let todoText = input.value;
+
+  if (todoText){
+  li.innerText = todoText;
+  li.classList.add("list-group-item");
+
+  ul.appendChild(li);
+  input.value = "";
+  }
+};
+
