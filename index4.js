5ac0ac91a7fa26fbcc9ed4e0502ee107c7c0b3a5
isukeaima <asaumi.kei49@gmail.com>
2021-12-13 17:56:38 +0900

    localStorageに保存しhtmlに表示

--- index4.js
+++ index4.js
@@ -1,6 +1,13 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos){
+  todos.forEach(todo => {
+    add(todo);
+  });
+}
 
 form.addEventListener("submit",function(event) {
   console.log(input.value);
@@ -8,9 +15,13 @@ form.addEventListener("submit",function(event) {
   add();
 });
 
-function add(){
+function add(todo){
   const li = document.createElement("li");
   let todoText = input.value;
+
+  if (todo){
+    todoText = todo;
+  }
 
   if (todoText){
   li.innerText = todoText;
@@ -23,5 +34,12 @@ function add(){
 };
 
 function saveData(){
-  
+const lists = document.querySelectorAll("li");
+let todos = [];
+
+lists.forEach(list => {
+  todos.push(list.innerText);
+});
+
+localStorage.setItem("todos",JSON.stringify(todos));
 }
